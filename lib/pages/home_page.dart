37c5b1609e0e6f27104d65683a21a91868d7a758
Arnaldo Junior <abs.junnior@hotmail.com>
2023-07-17 21:54:50 -0300

    Criação de layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:movie_catolog_app/models/movies_page.dart';
 import 'package:movie_catolog_app/repositories/movies_repository_imp.dart';
 import 'package:movie_catolog_app/services/dio_service_imp.dart';
 
+import '../utils/apis.utilis.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -23,20 +25,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ValueListenableBuilder<Movies?>(
-        //movies sera quem iremos observar
-        valueListenable: _controller.movies,
-        builder: (_, movies, __) {
-          return movies != null
-              ? ListView.builder(
-                  itemCount: movies.listOfMovies!.length,
-                  itemBuilder: (_, idx) =>
-                      Text(movies.listOfMovies![idx].title.toString()),
-                )
-              : Container(
-                  child: Text('falhou'),
-                );
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Movies',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              ValueListenableBuilder<Movies?>(
+                //movies sera quem iremos observar
+                valueListenable: _controller.movies,
+                builder: (_, movies, __) {
+                  return movies != null
+                      ? ListView.separated(
+                          physics: const NeverScrollableScrollPhysics(),
+                          separatorBuilder: (_, __) => const Divider(),
+                          shrinkWrap: true,
+                          itemCount: movies.listOfMovies!.length,
+                          itemBuilder: (_, idx) => Image.network(
+                            API.REQUEST_IMG(
+                              movies.listOfMovies![idx].posterPath.toString(),
+                            ),
+                          ),
+                        )
+                      : Container();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
