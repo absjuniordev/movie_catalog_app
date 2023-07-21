2c1e1e2f8b8917f295f3a7aac8e25e2620932991
Arnaldo Junior <abs.junnior@hotmail.com>
2023-07-20 21:48:35 -0300

    Update

--- lib/decorators/movies_cache_repository_decoratoor.dart
+++ lib/decorators/movies_cache_repository_decoratoor.dart
@@ -1,11 +1,36 @@
+import 'dart:convert';
+
 import 'package:movie_catolog_app/decorators/movies_repository_decorator.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/movies_page.dart';
 
 class MoviesCahceRepositoryDecorator extends MoviesRepositoryDecorator {
   MoviesCahceRepositoryDecorator(super.moviesRepository);
+
   @override
   Future<Movies> getMovies() async {
-    return super.getMovies();
+    try {
+      Movies movies = await super.getMovies();
+      _saveInCache(movies);
+      return movies;
+    } catch (e) {
+      return await _getInCahce();
+    }
+  }
+
+  _saveInCache(Movies movies) async {
+    var prefs = await SharedPreferences.getInstance();
+    String jsonMovies = jsonEncode((movies.toJson()));
+    prefs.setString('movies_cache', jsonMovies);
+  }
+
+  Future<Movies> _getInCahce() async {
+    var prefs = await SharedPreferences.getInstance();
+    var moviesJsonString = prefs.getString('movies_cache')!;
+    var json = jsonDecode(moviesJsonString);
+    var movies = Movies.fromJson(json);
+
+    return movies;
   }
 }
--- lib/decorators/movies_repository_decorator.dart
+++ lib/decorators/movies_repository_decorator.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:movie_catolog_app/models/movies_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../repositories/movies_repository.dart';
 
 class MoviesRepositoryDecorator implements MoviesRepository {
@@ -11,19 +8,5 @@ class MoviesRepositoryDecorator implements MoviesRepository {
   @override
   Future<Movies> getMovies() async {
     return await _moviesRepository.getMovies();
-  }
-
-  _saveInCache(Movies movies) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String jsonMovies = jsonEncode((movies.toJson()));
-    prefs.setString('movies_cache', jsonMovies);
-  }
-
-  Future<Movies> _getInCahce() async {
-    var prefs = await SharedPreferences.getInstance();
-    var moviesJsonString = prefs.getString('movies_cache')!;
-    var json = jsonDecode(moviesJsonString);
-
-    return Movies.fromJson(json);
   }
 }
